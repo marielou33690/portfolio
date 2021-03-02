a220392c1e9134d901c3b8228a44dfd0e93c22e6
Marie-Lou Denis <marielou33690@gmail.com>
2021-03-02 12:41:01 -0500

    configuration deploy

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,8 @@ module.exports = {
         additionalData: `@import "@/styles/_variables.scss";`
       }
     }
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/portfolio/'
+    : '/'
 };
